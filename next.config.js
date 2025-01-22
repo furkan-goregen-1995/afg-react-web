4ecccca55f45421ef4435c83b7aec68161bf3227
Furkan Göregen <furkan.goregen@stagedijital.com>
2025-01-23 01:18:04 +0300

    website deployed

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
-
-module.exports = nextConfig
+module.exports = {
+  async rewrites() {
+      return [
+          {
+              source: "/api/:path*", // Frontend çağrısı
+              destination: "http://manager.afg-react-web.com.tr/:path*", // Backend (HTTP)
+          },
+      ];
+  },
+};
