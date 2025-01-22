d11fb196d15da359802238396bfbbae4ac2b6d6c
Furkan Göregen <furkan.goregen@stagedijital.com>
2025-01-22 23:52:52 +0300

    website deployed

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,14 @@
 import Navbar from "./Navbar";
+import Head from 'next/head'
 
 
 export default function Layout({children}) {
   return (
     <div>
+        <Head>
+          <title>AFG React Laravel Web App</title>
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
+        </Head>
         <Navbar/>
         {children}
     </div>
